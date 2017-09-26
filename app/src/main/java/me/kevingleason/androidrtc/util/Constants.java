b8dfba63fd2b32c84cdd9f59ee4af7a3a9552ebc
Mmachi Obiorah <obiorahm@gmail.com>
2017-09-26 16:49:24 -0500

    new repository: video stream, webrtc, pubnub

---
+++ app/src/main/java/me/kevingleason/androidrtc/util/Constants.java
@@ -0,0 +1,33 @@
+package me.kevingleason.androidrtc.util;
+
+/**
+ * Created by GleasonK on 7/30/15.
+ */
+public class Constants {
+    public static final String SHARED_PREFS = "me.kg.androidrtc.SHARED_PREFS";
+    public static final String USER_NAME    = "me.kg.androidrtc.SHARED_PREFS.USER_NAME";
+    public static final String CALL_USER    = "me.kg.androidrtc.SHARED_PREFS.CALL_USER";
+    public static final String STDBY_SUFFIX = "-stdby";
+
+    public static final String PUB_KEY = "pub-c-c3bec981-532d-4201-8bda-b1764fde438a"; // Your Pub Key
+    public static final String SUB_KEY = "sub-c-4d13cdf2-a2d5-11e7-bb63-7e6e91347b5d"; // Your Sub Key
+
+//    public static final String PUB_KEY = "demo"; // Your Pub Key
+//    public static final String SUB_KEY = "demo"; // Your Sub Key
+
+    public static final String JSON_CALL_USER = "call_user";
+    public static final String JSON_CALL_TIME = "call_time";
+    public static final String JSON_OCCUPANCY = "occupancy";
+    public static final String JSON_STATUS    = "status";
+
+    // JSON for user messages
+    public static final String JSON_USER_MSG  = "user_message";
+    public static final String JSON_MSG_UUID  = "msg_uuid";
+    public static final String JSON_MSG       = "msg_message";
+    public static final String JSON_TIME      = "msg_timestamp";
+
+    public static final String STATUS_AVAILABLE = "Available";
+    public static final String STATUS_OFFLINE   = "Offline";
+    public static final String STATUS_BUSY      = "Busy";
+
+}
